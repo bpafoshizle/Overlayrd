0bbf57bbf35c639347da133cecd79899caea7cf2
bpafoshizle <bpafoshizle@gmail.com>
2023-03-18 00:45:48 -0500

    wip vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    ></v-navigation-drawer>
+    <v-main>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  //import OverlayCanvas from '@/components/OverlayCanvas.vue'
+</script>
